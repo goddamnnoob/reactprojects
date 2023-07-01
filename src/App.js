1746351a67fecdd566d45948e1cc8d658da5a765
goddamnnoob <gowthamreddy547@gmail.com>
2023-07-01 11:07:55 +0530

    Styling, Auto build using watch, variable

--- src/App.js
+++ src/App.js
@@ -2,29 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  function displayTime() {
-    try {
-      var region = document.getElementById("region").value;
-    console.log(region);
-    var date = new Date();
-    var currentTime = date.toLocaleString("en-US", {
-      timeZone: region
-    });
-    console.log(region);
-    document.getElementById("time").innerHTML = currentTime;
-    } catch (error) {
-      document.getElementById("time").innerHTML = error;
-    }
-  }
+  var user = "Mbapee"
   return (
-    <div className="App">
-    <h1>Current Time</h1>
-    <p >Enter region: </p>
-    <input type="text" id="region" name="region"/>
-    <button onClick={displayTime}>Submit</button>
-    <p id="time"></p>
-    </div>
-  );
+  <div>
+    <h1 className="MyButton">Hello World!!!</h1>
+    <h2>I AM {user}</h2>
+    <MyButton />
+  </div>);
+}
+
+function MyButton(){
+  return (<button className="MyButton">Help </button>);
 }
 
 
